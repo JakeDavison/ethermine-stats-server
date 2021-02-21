70d78036bcd2a1cf81ebd66c7539411d90b852c5
JakeDavison <jake.andrew.davison@gmail.com>
2021-02-21 22:15:48 +0000

    Configured port for heroku.

--- service/src/main/java/jakedavison/ethermine/stats/EthermineStatsApplication.java
+++ service/src/main/java/jakedavison/ethermine/stats/EthermineStatsApplication.java
@@ -4,9 +4,17 @@ import jakedavison.ethermine.stats.repository.AveragesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class EthermineStatsApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Ethermine Stats Server";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EthermineStatsApplication.class, args);
